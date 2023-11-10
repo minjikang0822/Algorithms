ad17006418c57208e992f5b31b445a837ce91288
minjikang0822 <minji1563@gmail.com>
2023-11-10 22:06:59 +0900

    delete fcn edited

--- 005_BinarySearchTree.py
+++ 005_BinarySearchTree.py
@@ -24,57 +24,66 @@ class BinarySearchTree:
             return self
         elif self.node < item:
             if self.right is not None:
-                self.right.search(item)
+                return self.right.search(item)
             else:
                 print(item, " doesn't exist")
                 return None
         elif self.node > item:
             if self.left is not None:
-                self.left.search(item)
+                return self.left.search(item)
             else:
                 print(item, " doesn't exist")
                 return None
 
+    def replace(self, to_replace, replace_children):
+        self.node = to_replace.node
+        if replace_children:
+            self.right = to_replace.right if to_replace.right is not None else None
+            self.left = to_replace.left if to_replace.left is not None else None
+
     def delete(self, item):
         node_to_delete = self.search(item)
-        print("hi", self.node)
         if node_to_delete is not None:
             # case 1: node to delete has two children
             if node_to_delete.left is not None and node_to_delete.right is not None:
-                node_to_replace = node_to_delete.right
-                # node to replace doesn't have a left child
-                if node_to_replace.left is None:
-                    node_to_delete.node = node_to_replace
-                else:
-                    while node_to_replace.left is not None:
-                        temp_parent = node_to_replace
-                        node_to_replace = node_to_replace.left
-                        if node_to_replace.left is None:
-                            # delete the node to replace from the tree
-                            temp_parent.left = None
-                    node_to_delete.node = node_to_replace.node
+                parent = node_to_delete
+                temp = node_to_delete.right
+                cnt = 0
+                while temp.left is not None:
+                    cnt += 1
+                    parent = temp
+                    temp = temp.left
+                node_to_delete.replace(temp, False)
+                if cnt != 0:
+                    parent.left = None
+                elif cnt == 0:
+                    parent.right = None
             else:
                 # case 2: node to delete has only one child
                 if node_to_delete.left is not None:
-                    node_to_delete.node = node_to_delete.left
-                # if right child is not None
+                    node_to_delete.replace(node_to_delete.left, True)
+                elif node_to_delete.right is not None:
+                    node_to_delete.replace(node_to_delete.right, True)
+                # case 3: no child
                 else:
-                    node_to_delete.node = node_to_delete.right
+                    # 부모찾아서 none해야함
+                    pass
 
     def printTree(self):
+        # 수정 필요
         print(self.node)
         print(self.left.node, ' ', self.right.node)
 
 
 def main():
     bst = BinarySearchTree(15)
-    to_insert = [8, 24, 5, 11, 2, 6, 13, 12, 19, 25, 28]
+    to_insert = [8, 24, 5, 13, 19, 28, 2, 6, 12, 25]
     for item in to_insert:
         bst.insert(item)
     bst.delete(11)
     bst.delete(24)
     bst.printTree()
-    print(bst.right.node)
+    print(bst.left.right.left.node)
 
 
 if __name__ == "__main__":
