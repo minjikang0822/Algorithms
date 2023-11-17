78591648d1c3b0398cf29a00ac4608363b23aa78
minjikang0822 <minji1563@gmail.com>
2023-11-17 22:44:11 +0900

    fenwickTree DONE

--- 007_FenwickTree.py
+++ 007_FenwickTree.py
@@ -5,11 +5,12 @@ print(9 & -2)
 
 
 # int & int -> AND operation between their binary values
-
+# Fenwick Tree index starts from 1
 
 def createFenwickTree(original_list, n):
-    print("what")
     fenwick_list = [0] * (n + 1)
+    # temp list would have i & -i for ith value
+    # it represents how many elements would we add up together to store at ith index of fenwickTree
     temp = [0] * (n+1)
     for i in range(1, n + 1, 1):
         # idx for original_list
@@ -26,27 +27,37 @@ def createFenwickTree(original_list, n):
             # except the i th element, it would be m-1 elements
             # -> need sum of elements from i-1-(m-1)th to i-1 th
             # --> i-1-(m-1) = i-1-m+1 = i-m
-            sum_before_i = fenwickSum(fenwick_list, i-m, i-1)
+            sum_before_i = fenwickSum(fenwick_list, i-1) - fenwickSum(fenwick_list, i-m)
             fenwick_list[i] = sum_before_i + original_list[j]
-    print(temp[1:])
-    return fenwick_list[1:]
+    # print(temp[1:])
+    return fenwick_list
 
 
-def fenwickSum(fenwick_list, start, end):
+def fenwickSum(fenwick_list, i):
     result = 0
-    i = end
-    print(start)
-    while i > start:
+    while i > 0:
         result += fenwick_list[i]
         i -= i & -i
     return result
+
+
+def fenwickUpdate(fenwick_list, i, to_add):
+    while i < len(fenwick_list):
+        fenwick_list[i] += to_add
+        i += i & -i
 
 
 def main():
     test_list = [5, 2, 9, -3, 5, 20, 10, -7, 2, 3, -4, 0, -2, 15, 5]
     print("Fenwick Tree")
     fenwickTree = createFenwickTree(test_list, len(test_list))
-    print(fenwickTree)
+    print(fenwickTree[1:])
+    # [5, 7, 9, 13, 5, 25, 10, 41, 2, 5, -4, 1, -2, 13, 5]
+    print(fenwickSum(fenwickTree, 6))
+    # 38
+    fenwickUpdate(fenwickTree, 4, 10)
+    print(fenwickTree[1:])
+    # [5, 7, 9, 23, 5, 25, 10, 51, 2, 5, -4, 1, -2, 13, 5]
 
 
 if __name__ == "__main__":
