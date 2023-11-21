7076fb93e76067a0a4beb0d3747a4e4883931120
minjikang0822 <minji1563@gmail.com>
2023-11-21 23:38:08 +0900

    merge descending ver and ascending ver together \n new example

--- 008_SelectionSort.py
+++ 008_SelectionSort.py
@@ -1,34 +1,31 @@
 # Ascending
-def selectionSort_A(to_sort):
-    for i in range(len(to_sort)):
-        min_idx = i
-        for j in range(i+1, len(to_sort), 1):
-            if to_sort[min_idx] > to_sort[j]:
-                min_idx = j
-        to_sort[i], to_sort[min_idx] = to_sort[min_idx], to_sort[i]
-
-
-# Descending
-def selectionSort_D(to_sort):
-    for i in range(len(to_sort)):
-        max_idx = i
-        for j in range(i + 1, len(to_sort), 1):
-            if to_sort[max_idx] < to_sort[j]:
-                max_idx = j
-        to_sort[i], to_sort[max_idx] = to_sort[max_idx], to_sort[i]
+def selectionSort(target, is_ascending=True):
+    if is_ascending:
+        for i in range(len(target)):
+            min_idx = i
+            for j in range(i+1, len(target), 1):
+                if target[min_idx] > target[j]:
+                    min_idx = j
+            target[i], target[min_idx] = target[min_idx], target[i]
+    else:
+        for i in range(len(target)):
+            max_idx = i
+            for j in range(i + 1, len(target), 1):
+                if target[max_idx] < target[j]:
+                    max_idx = j
+            target[i], target[max_idx] = target[max_idx], target[i]
+    return target
 
 
 def main():
-    init = [9, 2, 1, 5, 4, 0, 3, 8, 7, 6]
+    test = [-1, -5, 3, 60, 99, -200, 434, 52, 20, -11]
     print("Ascending Sorting -----")
-    selectionSort_A(init)
-    print(init)
-    # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(selectionSort(test))
+    # [-200, -11, -5, -1, 3, 20, 52, 60, 99, 434]
 
     print("Descending Sorting -----")
-    selectionSort_D(init)
-    print(init)
-    # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    print(selectionSort(test, False))
+    # [434, 99, 60, 52, 20, 3, -1, -5, -11, -200]
 
 
 if __name__ == "__main__":
