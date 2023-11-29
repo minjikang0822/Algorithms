29771a94e2dd1423efafddb1782e28efb9a3a60b
minjikang0822 <minji1563@gmail.com>
2023-11-30 00:18:29 +0900

    linearSearch DONE

--- 026_LinearSearch.py
+++ 026_LinearSearch.py
@@ -1,5 +1,18 @@
+def linearSearch(target_list, what_to_find):
+    for i in range(len(target_list)):
+        if target_list[i] == what_to_find:
+            print(what_to_find, "found at index", i)
+            return i
+    print(what_to_find, "not in the list")
+    return -1
+
+
 def main():
-    pass
+    target_list = [2, -2, 10, 9, 22, 5, 6, 15, 99, 45, 69, 32]
+    linearSearch(target_list, 1)
+    # 1 not in the list
+    linearSearch(target_list, 99)
+    # 99 found at index 8
 
 
 if __name__ == "__main__":
