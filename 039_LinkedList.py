7d0bd5657a4cc4c3534dd52e9c5a2326eccef252
minjikang0822 <minji1563@gmail.com>
2023-12-07 00:06:09 +0900

    linkedList init

--- 039_LinkedList.py
+++ 039_LinkedList.py
@@ -1,3 +1,35 @@
+class SinglyLinkedList:
+    def __init__(self, node, nextNode=None):
+        self.node = node
+        self.nextNode = nextNode
+
+
+class DoublyLinkedList:
+    def __init__(self, node, nextNode, previousNode):
+        self.node = node
+        self.nextNode = nextNode
+        self.previousNode = previousNode
+
+
+class CircularSinglyLinkedList:
+    def __init__(self, node, nextNode):
+        self.node = node
+        self.nextNode = nextNode
+
+    def insertion(self):
+        pass
+
+    def deletion(self):
+        pass
+
+
+class CircularDoublyLinkedList:
+    def __init__(self, node, nextNode, previousNode):
+        self.node = node
+        self.nextNode = nextNode
+        self.previousNode = previousNode
+
+
 def main():
     pass
 
