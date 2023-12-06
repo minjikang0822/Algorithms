eb12a8e94563fdee8841938a2e6f7b508a32c54d
minjikang0822 <minji1563@gmail.com>
2023-12-06 23:17:35 +0900

    ternarySearch with recursion DONE

--- 030_TernarySearch.py
+++ 030_TernarySearch.py
@@ -1,5 +1,29 @@
+def ternarySearch(target, to_find):
+    if len(target) < 2:
+        print(to_find, "does not exists")
+        return -1
+    l = 0
+    r = len(target)-1
+    m1 = l + (r - l) // 3
+    m2 = r - (r - l) // 3
+    print(target)
+    print(m1)
+    print(m2)
+    if target[m1] == to_find:
+        return m1
+    elif target[m2] == to_find:
+        return m2
+    elif target[m1] > to_find:
+        return ternarySearch(target[:m1], to_find)
+    elif target[m2] < to_find:
+        return ternarySearch(target[m2 + 1:], to_find)
+    elif target[m1] < to_find < target[m2]:
+        return ternarySearch(target[m1 + 1:m2], to_find)
+
+
 def main():
-    pass
+    test_target = [-7, -3, 0, 2, 4, 7, 11, 20, 25, 34]
+    print(ternarySearch(test_target, to_find=5))
 
 
 if __name__ == "__main__":
