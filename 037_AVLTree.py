7e66dda27f13685d7d2f3800524f107060ce31bc
minjikang0822 <minji1563@gmail.com>
2023-12-09 20:33:18 +0900

    avl tree init

--- 037_AVLTree.py
+++ 037_AVLTree.py
@@ -1,3 +1,22 @@
+# self-balancing binary search tree
+class AVLTree:
+    def __init__(self, node, left=None, right=None):
+        self.node = node
+        self.left = left
+        self.right = right
+        self.height = 1
+        self.balance = self.left.height - self.right.height
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def search(self):
+        pass
+
+
 def main():
     pass
 
