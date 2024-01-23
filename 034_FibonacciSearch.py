d25a945475c1d44a25aefb092643ee83a2cb40e3
minjikang0822 <minji1563@gmail.com>
2024-01-23 23:24:23 +0900

    “update”

--- 034_FibonacciSearch.py
+++ 034_FibonacciSearch.py
@@ -1,6 +1,13 @@
 def fibonacciSearch(arr):
     n = len(arr)
 
+
+def fibonnaciNumber(n):
+    if n <= 2:
+        return 1
+    return fibonnaciNumber(n-2) + fibonnaciNumber(n-1)
+
+
 def main():
     pass
 
