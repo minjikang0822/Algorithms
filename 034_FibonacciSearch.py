29d4c2b27bd0df6f7e576d6f13be5e899fd0a311
minjikang0822 <minji1563@gmail.com>
2024-01-23 23:54:11 +0900

    “update”

--- 034_FibonacciSearch.py
+++ 034_FibonacciSearch.py
@@ -1,10 +1,16 @@
-def fibonacciSearch(arr):
+def fibonacciSearch(arr, to_find):
     n = len(arr)
     fibonacci_list = fibonacciNumbers(n)
     k = len(fibonacci_list)
     Fk = fibonacci_list[k]
     Fk1 = fibonacci_list[k-1]
     Fk2 = fibonacci_list[k-2]
+    offset = -1
+    idx = min(offset + Fk2, n-1)
+
+    while arr[idx] != to_find:
+        if arr[idx] > to_find:
+            pass
 
 
 def fibonacciNumbers(n):
@@ -19,7 +25,7 @@ def fibonacciNumbers(n):
 
 def main():
     test_arr = [1, 2, 4, 5, 6, 10]
-    fibonacciSearch(test_arr)
+    fibonacciSearch(test_arr, 5)
 
 
 if __name__ == "__main__":
