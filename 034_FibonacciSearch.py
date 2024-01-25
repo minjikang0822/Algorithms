d18778119ea1d7915dbc0285ecdb1e8d491f1e62
minjikang0822 <minji1563@gmail.com>
2024-01-25 15:56:47 +0900

    fibonacci search done

--- 034_FibonacciSearch.py
+++ 034_FibonacciSearch.py
@@ -1,21 +1,26 @@
 def fibonacciSearch(arr, to_find):
     n = len(arr)
     fibonacci_list = fibonacciNumbers(n)
-    k = len(fibonacci_list)
-    Fk = fibonacci_list[k]
-    Fk1 = fibonacci_list[k-1]
-    Fk2 = fibonacci_list[k-2]
+    m = len(fibonacci_list) - 1
     offset = -1
-    idx = min(offset + Fk2, n-1)
 
-    while arr[idx] != to_find:
+    while fibonacci_list[m] > 1:
+        Fm2 = fibonacci_list[m - 2]
+        idx = min(offset + Fm2, n - 1)
+
         if arr[idx] > to_find:
-            pass
+            m = m - 2
+        elif arr[idx] < to_find:
+            m = m - 1
+            offset = idx
+        else:
+            return idx
+
+    return -1
 
 
 def fibonacciNumbers(n):
     fibonacci_list = [0, 1]
-
     i = 2
     while fibonacci_list[-1] < n:
         fibonacci_list.append(fibonacci_list[i - 2] + fibonacci_list[i - 1])
@@ -24,8 +29,18 @@ def fibonacciNumbers(n):
 
 
 def main():
-    test_arr = [1, 2, 4, 5, 6, 10]
-    fibonacciSearch(test_arr, 5)
+    test_arr = [12, 14, 16, 17, 20, 24, 31, 43, 50, 62]
+    idx1 = fibonacciSearch(test_arr, 24)
+    print(idx1)
+    # 5
+
+    idx2 = fibonacciSearch(test_arr, 12)
+    print(idx2)
+    # 0
+
+    idx3 = fibonacciSearch(test_arr, 62)
+    print(idx3)
+    # 9
 
 
 if __name__ == "__main__":
