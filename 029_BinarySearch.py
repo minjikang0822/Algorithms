260bf52cc5467f6126750a8814e441d13268b08a
minjikang0822 <minji1563@gmail.com>
2024-01-25 16:22:25 +0900

    binarySearch implemented

--- 029_BinarySearch.py
+++ 029_BinarySearch.py
@@ -1,3 +1,23 @@
+def binarySearch(sorted_arr, to_find):
+    n = len(sorted_arr) - 1
+    start = 0
+    mid = n//2
+    end = n
+
+    while 0 <= mid <= n:
+        if sorted_arr[mid] > to_find:
+            end = mid
+            mid = mid//2
+        elif sorted_arr[mid] < to_find:
+            start = mid
+            mid = (end - start) // 2
+        else:
+            return mid
+
+    # item not found
+    return -1
+
+
 def main():
     pass
 
