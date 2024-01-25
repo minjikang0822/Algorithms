77abc4cdf701b91f79b724cb40ea2fcd0d263394
minjikang0822 <minji1563@gmail.com>
2024-01-25 16:56:24 +0900

    bianry search DONE

--- 029_BinarySearch.py
+++ 029_BinarySearch.py
@@ -1,9 +1,9 @@
 def binarySearch(sorted_arr, to_find):
     left = 0
     right = len(sorted_arr) - 1
-    mid = (right - left) // 2
 
     while left < right:
+        mid = left + (right - left) // 2
         print("left:", left, "mid:", mid, "right:", right)
         if sorted_arr[mid] > to_find:
             right = mid
@@ -11,7 +11,6 @@ def binarySearch(sorted_arr, to_find):
             left = mid
         else:
             return mid
-        mid = mid + (right - left) // 2
 
     # item not found
     return -1
@@ -19,8 +18,12 @@ def binarySearch(sorted_arr, to_find):
 
 def main():
     test_arr = [-10, -5, -2, -1, 0, 2, 4, 6, 12, 13, 14]
+
     print(binarySearch(test_arr, 12))
     # 8
+
+    print(binarySearch(test_arr, -2))
+    # 2
 
 
 if __name__ == "__main__":
