389fb056902ca9e86ed26b1e36ca0dcfff899c97
minjikang0822 <minji1563@gmail.com>
2024-01-29 20:40:17 +0900

    update

--- 031_JumpSearch.py
+++ 031_JumpSearch.py
@@ -3,23 +3,22 @@ import math
 
 def jumpSearch(arr, to_find):
     n = len(arr)
+    print("n:", n)
     # size of block
     m = int(math.sqrt(n))
 
     k = 0
     while k * m < n:
         idx = k * m
+        next_idx = min((k + 1) * m, n - 1)
 
         if arr[idx] == to_find:
             return idx
-
-        elif arr[idx] > to_find:
-            temp_idx = (k-1) * m
-            while temp_idx < idx:
-                if arr[temp_idx] == to_find:
-                    return temp_idx
-                temp_idx += 1
-            break
+        elif arr[next_idx] > to_find:
+            while idx < next_idx:
+                if arr[idx] == to_find:
+                    return idx
+                idx += 1
         k += 1
     return -1
 
@@ -39,6 +38,9 @@ def main():
     # 10
 
     print(jumpSearch(test_arr, 10))
+    # 9
+
+    print(jumpSearch(test_arr, -8))
     # 9
 
 
